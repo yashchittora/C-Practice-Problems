4d36442eab23bbe15fd8b4e2d056538de88c17ac
Yash Chittora <yashchittora.code@gmail.com>
2022-12-16 22:18:18 +0530

    Add num 1

---
+++ 02-Patterns/num-1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	int n = 5;
+
+	for (int i = 1; i<=n; i++){
+		for (int j = 1; j<=i; j++){
+			printf("%d ",j);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
+
+
+// 1 
+// 1 2 
+// 1 2 3 
+// 1 2 3 4 
+// 1 2 3 4 5 
