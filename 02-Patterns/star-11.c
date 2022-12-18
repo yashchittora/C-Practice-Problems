492ef041095ae13c3dbe038a8d7184b4d78c0362
Yash Chittora <yashchittora.code@gmail.com>
2022-12-18 19:41:44 +0530

    Add star 11

---
+++ 02-Patterns/star-11.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int n = 5;
+    
+    for (int i=n; i>=1; i--){
+        for (int j=1; j<=n; j++){
+            if (j <= n-i){
+                printf(" ");
+            }
+            
+            else if (j>n-i) {
+                printf("* ");
+            }
+            
+        }
+    
+        
+        printf("\n");
+    }
+    
+
+    return 0;
+}
+
+
+// * * * * * 
+//  * * * * 
+//   * * * 
+//    * * 
+//     * 
+
+
